78303361aad34c3900b793a030a9c6f46e07a5d4
Fiona McCarthy <fmccarthy@pitp.ca>
2018-09-21 14:41:42 -0400

    test some ring properties

--- src/Example2018.jl
+++ src/Example2018.jl
@@ -36,7 +36,7 @@ end
 
 function Base. +(p::Polynomial, q::Polynomial)
     l = max(length(p.coeffs), length(q.coeffs))
-    rcoeffs = zeros(l)
+    rcoeffs = zeros(eltype(p.coeffs),l)
     for (i,c) in enumerate(p.coeffs)
         rcoeffs[i] += c
     end
@@ -45,6 +45,20 @@ function Base. +(p::Polynomial, q::Polynomial)
     end
     Polynomial(rcoeffs)
 end
+
+function Base. -(p::Polynomial)
+	 (-1)*p	 
+end
+
+function Base. -(p::Polynomial,q::Polynomial)
+	 p+(-q)
+end
+
+function Base. ==(p::Polynomial, q::Polynomial)
+	 d=p-q
+	 all(c->c==0,d.coeffs)
+end
+
 
 function Base. *(a, p::Polynomial)
     Polynomial(map(x->a*x, p.coeffs))
